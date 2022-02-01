bcbfe3697fb39be1a068d3549b7119b8d374d26f
horyukova <horyukova@yoomoney.ru>
2022-02-01 21:48:55 +0300

    Обход файлов сделан рекурсивным

--- src/main/kotlin/ru/yoomoney/gradle/plugins/moira/trigger/collect/DirectoryCollector.kt
+++ src/main/kotlin/ru/yoomoney/gradle/plugins/moira/trigger/collect/DirectoryCollector.kt
@@ -1,7 +1,6 @@
 package ru.yoomoney.gradle.plugins.moira.trigger.collect
 
 import java.io.File
-import java.io.FileFilter
 
 /**
  * Collects items from directory using recursive search.
@@ -9,26 +8,15 @@ import java.io.FileFilter
  * Note that [maxDepth] = 1 means that only root will be scanned. Depth is unlimited by default.
  */
 class DirectoryCollector<C>(
-    private val collector: FilesCollector<C>,
-    private val recursive: Boolean = true,
-    private val maxDepth: Int = Integer.MAX_VALUE
+    private val collector: FilesCollector<C>
 ) : FilesCollector<List<C>> {
 
     override fun isSupported(file: File): Boolean = file.isDirectory
 
-    override fun collect(file: File): List<C> = collectRecursive(file, 0)
-
-    private fun collectRecursive(file: File, depth: Int): List<C> {
-        if (depth >= maxDepth) {
-            return emptyList()
-        }
-        val files = file.listFiles(FileFilter { collector.isSupported(it) })
-
-        var collectedItems = files.map { collector.collect(it) }
-        if (recursive) {
-            val directories = file.listFiles(FileFilter { it.isDirectory })
-            collectedItems += directories.flatMap { collectRecursive(it, depth + 1) }
-        }
-        return collectedItems
+    override fun collect(file: File): List<C> {
+        return file.walkTopDown()
+            .filter { collector.isSupported(it) && !it.isDirectory }
+            .map { collector.collect(it) }
+            .toList()
     }
 }
